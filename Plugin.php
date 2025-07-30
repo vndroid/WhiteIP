9aded05400010ce7f465bbf1a1e18f0087f567ad
vndroid <diracsvip@gmail.com>
2025-07-30 23:31:08 +0800

    Update
    - 补充资源

---
+++ Plugin.php
@@ -0,0 +1,101 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * 管理后台 IP 白名单
+ * 
+ * @package WhiteIP
+ * @author Cain
+ * @version 1.0.0
+ * @link https://github.com/Vndroid/WhiteIP
+ */
+class WhiteIP_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+
+    public static function activate()
+    {
+        Typecho_Plugin::factory('admin/common.php')->begin = array('WhiteIP_Plugin', 'check');
+        Typecho_Plugin::factory('Widget_Login')->loginSucceed = array('WhiteIP_Plugin', 'check');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        /** 允许登陆后台的 IP */
+        $allow_ip = new Typecho_Widget_Helper_Form_Element_Text('allow_ip', NULL, NULL, _t('后台管理IP白名单'),'请输入 IP 地址，如果有多个请使用英文逗号隔开');
+        $form->addInput($allow_ip);
+        /** 跳转链接 */
+        $location_url = new Typecho_Widget_Helper_Form_Element_Text('location_url', NULL, 'https://www.google.com/', _t('跳转链接'),'请输入标准的 URL 地址（包括 http://），白名单外的 IP 访问后台将会跳转至这个 URL');
+        $form->addInput($location_url);
+    }
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 检测 IP 白名单
+     * 
+     * @access public
+     * @return void
+     */
+    public static function check()
+    {
+        static $real_ip = NULL;
+        // 判断服务器是否允许 $_SERVER ，不允许则使用 getenv 获取
+        $real_ip = isset($_SERVER) ? $_SERVER['REMOTE_ADDR'] : getenv("REMOTE_ADDR");
+
+        if($real_ip !== NULL){
+            $config = json_decode(json_encode(unserialize(Helper::options()->plugin('WhiteIP'))));
+            if(empty($config->allow_ip)) {
+                $options = Typecho_Widget::widget('Widget_Options');
+                $config_url = trim($options->siteUrl,'/').'/'.trim(__TYPECHO_ADMIN_DIR__,'/').'/options-plugin.php?config=WhiteIP';
+                echo '请先进行设置可访问后台白名单，<a href="'.$config_url.'">马上去设置</a>';
+            } else {
+                $allow_ip_arr = str_replace('，',',',$config->allow_ip);
+                $allow_ip = explode(',', $allow_ip_arr);
+                
+                // 误操作紧急通道，去掉下行注释即可放行全部地址
+                //$allow_ip[] = '0.0.0.0';
+                
+                $location_url = trim($config->location_url) ? trim($config->location_url) : 'https://www.google.com/';
+                if(!in_array('0.0.0.0', $allow_ip)) {
+                    if(!in_array($real_ip, $allow_ip)) {
+                        Typecho_Cookie::delete('__typecho_uid');
+                        Typecho_Cookie::delete('__typecho_authCode');
+                        @session_destroy();
+                        header('Location: '.$location_url);
+                        exit;
+                    }
+                }
+            }
+        }
+    }
+}
